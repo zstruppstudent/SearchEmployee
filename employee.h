175e46cd05937edb163a29bd2e49860b7f1ea87c
Zac Strupp <zistrupp@cpp.edu>
2023-10-27 18:24:31 -0700

    Finished changes for part 1a

--- employee.h
+++ employee.h
@@ -1,16 +1,13 @@
 #include <stdio.h> 
-
 #include <stddef.h> 
 
-typedef struct { 
-
+typedef struct 
+{ 
     long number; 
     char *name; 
     char *phone; 
     double salary; 
 
 } Employee, *PtrToEmployee; 
-
- 
 
 typedef const Employee *PtrToConstEmployee; 
